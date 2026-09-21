d716ef37b7882b212ab4698595434c1103713815
unovyx <unovyx@gmail.com>
2026-09-22 04:46:56 +0800

    Fix CORS for Vercel and localhost

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -11,7 +11,8 @@ const app = express();
 
 const allowedOrigins = [
     "https://mysandbox-six.vercel.app",
-    "http://localhost:5500"
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
 ];
 
 app.use(cors({
